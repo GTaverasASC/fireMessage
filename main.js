9ff786cf7be091900d93cf5ada990d3e3cd87eed
Giovanni Taveras <gtaveras@browning.edu>
2019-08-07 11:48:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const usernameElement = document.getElementById("username");
+const messageElement = document.getElementById("message");
+const button = document.getElementById("submitButton");
+const allMessages = document.getElementsByClassName("allMessages")[0];
+button.addEventListener("click", updateDB);
+
+//Set database object here
+var db = firebase.database().ref("message");
+
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event) {
+  event.preventDefault();
+  const username = usernameElement.value;
+  const message = messageElement.value;
+
+  usernameElement.value = "";
+  messageElement.value = "";
+
+  console.log(username + " : " + message);
+
+  //Update database here
+  const value = {
+    username: username,
+    message: message
+  };
+  db.push(value);
+}
+
+// Set database "child_added" event listener here
+db.on("child_added", function(dataRow) {
+  const value = dataRow.val();
+
+  const p = document.createElement("p");
+  p.innerText = `${value.username}: ${value.message}`;
+  allMessages.appendChild(p);
+});
